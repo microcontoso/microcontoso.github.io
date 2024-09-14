085d2691bcab5f0f6776b862a203045a8a3fa8c6
MiniContoso <minicontoso@outlook.com>
2024-09-14 20:42:49 +0800

    echarts

---
+++ echarts/configs/config.gh.js
@@ -0,0 +1,10 @@
+export default {
+  routerBase: '/echarts-handbook',
+  rootPath: 'https://apache.github.io/echarts-handbook/',
+  exampleViewPath: 'https://echarts.apache.org/examples/${lang}/view.html?c=',
+  exampleEditorPath:
+    'https://echarts.apache.org/examples/${lang}/editor.html?c=',
+  mainSitePath: 'https://echarts.apache.org/',
+  optionPath: 'https://echarts.apache.org/option.html#',
+  gitRepo: 'apache/echarts-handbook'
+}
